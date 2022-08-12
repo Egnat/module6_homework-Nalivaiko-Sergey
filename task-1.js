d39001d7c8f30f321fe8ebe647c884c96fa52c22
Egnat <sergey_p_i@mail.ru>
2022-08-13 00:13:17 +0300

    (Modil 6 JS task-1 master)

--- task-1.js
+++ task-1.js
@@ -1,8 +1,8 @@
 //Если убрать ! после %3 то получится - не число. А какой ответ лучше?
 function different(num, str, sing) { 
-if ('different' %3 !== 1) {
+if (different %3 !== 1) {
 console.log("odd");
-}else if ('different' %2 == 2) {
+}else if (different %2 == 2) {
 console.log('even');
 }else {
 console.log('значит не число'); 
