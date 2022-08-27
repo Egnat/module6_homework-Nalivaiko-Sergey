9659919c9258eab51f263b4682a85d91b7d9cb98
Egnat <sergey_p_i@mail.ru>
2022-08-28 02:09:44 +0300

    (master js)

--- task-3.js
+++ task-3.js
@@ -1,9 +1,14 @@
+/*Написать функцию, которая принимает число как аргумент и возвращает функцию, 
+которая также принимает число как аргумент и возвращает сумму этих двух чисел. 
+Выведите в консоль результат.*/
+
 function getNum(sum) {
-sum();
-return (sum)  
+  sum();
+  return (sum)  
 }
-function argSum(){  
-console.log(2+1);   
+function argSum() {  
+  console.log(2+1);   
 } 
 const resultArgSum = getNum(argSum)
+
 resultArgSum();
